db7b68b25d6b7b5f27736dd802a34b85d8bb89ac
LakhaniSinan <sinan.lakhani09@gmail.com>
2026-07-02 00:31:33 +0500

    cc

--- src/config/server.js
+++ src/config/server.js
@@ -1,20 +1,20 @@
 export const API_BASE_URL = {
-  STAGING_API_BASE_URL:
-    'https://evenlyo-backend-20036df510ad.herokuapp.com/api',
+  // STAGING_API_BASE_URL:
+  //   'https://evenlyo-backend-20036df510ad.herokuapp.com/api',
   LIVE_API_BASE_URL:
     'https://evenlyo-backend-production-7482992ce6c7.herokuapp.com/api',
   // LOCAL_API_BASE_URL: 'https://9dbm90sc-4040.inc1.devtunnels.ms/api',
-  LOCAL_API_BASE_URL: 'https://prhfz1x9-4040.euw.devtunnels.ms/api',
-  LOCAL_API_BASE_URL1: 'https://3znr6k1s-4040.inc1.devtunnels.ms/api',
-  LOCAL_API_BASE_URL2: 'https://9dbm90sc-4040.inc1.devtunnels.ms/api',
+  // LOCAL_API_BASE_URL: 'https://prhfz1x9-4040.euw.devtunnels.ms/api',
+  // LOCAL_API_BASE_URL1: 'https://3znr6k1s-4040.inc1.devtunnels.ms/api',
+  // LOCAL_API_BASE_URL2: 'https://9dbm90sc-4040.inc1.devtunnels.ms/api',
 };
 export const SOCKET_BASE_URL = {
-  STAGING_SOCKET_BASE_URL: 'https://evenlyo-backend-20036df510ad.herokuapp.com',
+  // STAGING_SOCKET_BASE_URL: 'https://evenlyo-backend-20036df510ad.herokuapp.com',
   LIVE_SOCKET_BASE_URL:
     'https://evenlyo-backend-production-7482992ce6c7.herokuapp.com',
-  LOCAL_SOCKET_BASE_URL: 'https://prhfz1x9-4040.euw.devtunnels.ms',
-  LOCAL_SOCKET_BASE_URL1: 'https://3znr6k1s-4040.inc1.devtunnels.ms',
-  LOCAL_SOCKET_BASE_URL2: 'https://9dbm90sc-4040.inc1.devtunnels.ms',
+  // LOCAL_SOCKET_BASE_URL: 'https://prhfz1x9-4040.euw.devtunnels.ms',
+  // LOCAL_SOCKET_BASE_URL1: 'https://3znr6k1s-4040.inc1.devtunnels.ms',
+  // LOCAL_SOCKET_BASE_URL2: 'https://9dbm90sc-4040.inc1.devtunnels.ms',
 };
 export const STRIPE_PUBLISHABLE_KEYS = {
   TEST: 'pk_test_51S5mMXIUXgoWiMw14oUpuKyQawd4L7FDZNzS7O99qwoERe5PBh9lTVlc38G3AMKDvHIdMmIQa6NHfs5IvG8zacPy00P8gvRAl9',
